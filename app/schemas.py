1a0632bc4ff6774cbd74ee0a405d656cf509015e
JAngelGL <angel_garcia360@hotmail.com>
2026-02-21 11:23:12 -0600

    If this commit applies will change the class ItemResponse

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 class ItemBase(BaseModel):
     name: str = Field(..., min_length=2)
@@ -15,5 +15,4 @@ class ItemUpdate(ItemBase):
 class ItemResponse(ItemBase):
     id: int
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
